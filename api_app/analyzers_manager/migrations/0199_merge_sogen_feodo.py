8505052316d0d8d26941d5367c516a4e6a95535c
nick <vishwajeetshah76@gmail.com>
2026-09-16 10:53:42 +0530

    chore(sogen): merge migration graph, resolve conflicting leaf nodes

---
+++ api_app/analyzers_manager/migrations/0199_merge_sogen_feodo.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("analyzers_manager", "0197_analyzer_config_sogen"),
+        ("analyzers_manager", "0198_remove_feodo_tracker_analyzer"),
+    ]
+
+    operations = []
